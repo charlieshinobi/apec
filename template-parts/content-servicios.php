48a8e7ff415b7cf12296315864829805c2cd7b35
Charlie <ramc.rc@gmail.com>
2019-06-11 15:10:34 -0500

    agregar hover en descripcion de servicios

--- template-parts/content-servicios.php
+++ template-parts/content-servicios.php
@@ -9,27 +9,24 @@
       </div>
     </div>
     <div class="row">
-
       <?php $loop = new WP_Query( array ('post_type' => 'servicios_home', 'orderby' => 'post_date', 'order' => 'ASC') ); ?>
       <?php
         $cont = 0;
       while($loop->have_posts() ) : $loop->the_post();
-
         $servicios_link   = get_field('servicios_link');
         $cont++;
         $name_class = "serv".$cont;
       ?>
       <a href="<?php echo $servicios_link;?>">
-
         <div class='col-md-4 serv'>
           <?php the_post_thumbnail();?>
           <div class="tit"><?php the_title(); ?></div>
+          <div class="desc"><?php the_content(); ?></div>
         </div>
       </a>
-
       <?php endwhile; wp_reset_query();?>
-
     </div>
   </div>
+
 
 </section>
